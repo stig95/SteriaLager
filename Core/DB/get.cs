de11ed91a031d859759bdabb3b8852fc9d6f525f
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-06-12 22:08:56 +0200

    App.config start

---
+++ Core/DB/get.cs
@@ -0,0 +1,16 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Core.DB
+{
+    public static class get
+    {
+       
+    }
+}
